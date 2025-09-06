86584385587d7c2f2c890ac36b386c2b734a24f0
Bryan Choate <bryan.choate@gmail.com>
2025-09-06 10:31:57 -0400

    Add permutations to combos

--- combos.py
+++ combos.py
@@ -1,19 +1,32 @@
 import math
 
-def calculate_combos(n: int, k: int) -> int:
-    if k > n or k < 0:
-        print("Invalid input: k must be between 0 and n.")
-        return 0
+
+def calculate_combinations(n: int, k: int) -> int:
     return math.factorial(n) // (math.factorial(k) * math.factorial(n - k))
+
+
+def calculate_permutations(n: int, k: int) -> int:
+    return math.factorial(n) // math.factorial(n - k)
+
 
 def main():
     try:
         n = int(input("Enter total number of items (n): "))
         k = int(input("Enter number of items to choose (k): "))
-        combos = calculate_combos(n, k)
-        print(f"Number of ways to choose {k} items from {n} items: {combos}")
     except ValueError:
         print("Invalid input: Please enter integer values for n and k.")
+        exit(1)
+
+    if k > n or k < 0:
+        print("Invalid input: k must be between 0 and n.")
+        exit(1)
+
+    perms = format(calculate_permutations(n, k), ",d")
+    print(f"Number of ways to ARRANGE {k} items from {n} items: {perms}")
+
+    combinations = format(calculate_combinations(n, k), ",d")
+    print(f"Number of ways to CHOOSE {k} items from {n} items: {combinations}")
+
 
 if __name__ == "__main__":
     main()
